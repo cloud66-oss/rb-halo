088ce9153d567c41143d3eb21872abc8d38c09d1
Khash Sajadi <khash@cloud66.com>
2020-06-25 13:41:05 -0700

    initial

---
+++ lib/rb/halo.rb
@@ -0,0 +1,8 @@
+require "rb/halo/version"
+
+module Rb
+  module Halo
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rb/halo/version.rb
@@ -0,0 +1,5 @@
+module Rb
+  module Halo
+    VERSION = "0.1.0"
+  end
+end
